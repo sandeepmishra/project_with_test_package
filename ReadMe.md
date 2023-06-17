12b3db60b8891d55d8c9e4f5f8dd3c02b67a8a07
sandeep mishra <sandeepmishra023@gmail.com>
2023-06-17 18:01:27 +0530

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+From Root directory of package
+    . venv/bin/activate
+    cd requirements 
+    pip install -r test_requirements.txt 
+From Root Directory
+    cd titanic_model
+    python3 train_pipeline.py
+from the root directory of project
+    pytest
+from the root directory 
+    python3 -m pip install --upgrade build   
+    python3 -m build
+
+
+
+To add the project at github
+
+1. create git repo on your profile at github
+2. go to local git project and run ```git init``` to the project root directory
+3. git add
+4. git commit -m
+5. git remote add origin repo-url
+6. git push -u origin master
---
+++ tests/test_predictions.py
@@ -0,0 +1,31 @@
+"""
+Note: These tests will fail if you have not first trained the model.
+"""
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+from titanic_model.predict import make_prediction
+
+
+def test_make_prediction(sample_input_data):
+    # Given
+    expected_no_predictions = 179
+
+    # When
+    result = make_prediction(input_data=sample_input_data)
+
+    # Then
+    predictions = result.get("predictions")
+    assert isinstance(predictions, np.ndarray)
+    assert isinstance(predictions[0], np.int64)
+    assert result.get("errors") is None
+    assert len(predictions) == expected_no_predictions
+    _predictions = list(predictions)
+    y_true = sample_input_data["Survived"]
+    accuracy = accuracy_score(_predictions, y_true)
+    assert accuracy > 0.8
+
